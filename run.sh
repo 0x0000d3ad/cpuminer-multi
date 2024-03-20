56f9ac53a0bc3c6c95f1652d5afb589970536cd0
Quant Guy <148646431+KovachTechnologies@users.noreply.github.com>
2024-03-20 14:06:41 -0400

    Update run.sh
    updating hostname

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-./cpuminer -a scrypt -o stratum+tcp://scrypt.na.mine.zergpool.com:3433 -u DSvMzJQvThSCZwKwLBpiLuHSK6ce7LaESj -p c=DOGE,mc=DOGE,ID=$hostname
+./cpuminer -a scrypt -o stratum+tcp://scrypt.na.mine.zergpool.com:3433 -u DSvMzJQvThSCZwKwLBpiLuHSK6ce7LaESj -p c=DOGE,mc=DOGE,ID=$HOSTNAME
