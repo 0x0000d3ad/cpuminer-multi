ff20ecd9cf95559044486388c17ac7c152621e00
Quant Guy <148646431+KovachTechnologies@users.noreply.github.com>
2024-03-20 16:53:14 -0400

    Update run.sh
    updated run.sh for cryptonight algo that works

--- run.sh
+++ run.sh
@@ -1,1 +1,3 @@
-./cpuminer -a scrypt -o stratum+tcp://scrypt.na.mine.zergpool.com:3433 -u DSvMzJQvThSCZwKwLBpiLuHSK6ce7LaESj -p c=DOGE,mc=DOGE,ID=$HOSTNAME
+cd /home/daniel/cpuminer-multi
+dhclient eth0
+./cpuminer -a cryptonight -o stratum+tcp://cryptonight_upx.na.mine.zergpool.com:4457 -u DSvMzJQvThSCZwKwLBpiLuHSK6ce7LaESj -p c=DOGE,mc=UPX,ID=$HOSTNAME
